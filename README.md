9baa4d3c391b242e80777c931ec78fe99100a453
Kevin <kelvinngechu@gmail.com>
2022-11-28 14:19:02 +0300

    Q1 and Q2

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # phase-1code-challenge
-first Individual practice code challenge
+This is an individual labwork where I am supposed to complete a set of three given challenges:
+CHALLENGE NO. 1
+Write a program that prompts the user to input student marks. The input should be between 0 and 100. Then output the correct grade: 
+The first thing to do is to first declare variables a and 
+
+
 
--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-/* Write that prompts the user to input student marks. The input should be between 0 and 100. Then output the correct grade: 
+
+
+/* Challenge 1: Write that prompts the user to input student marks. The input should be between 0 and 100. Then output the correct grade: 
 
 A > 79, B - 60 to 79, C -  59 to 49, D - 40 to 49, E - less 40.*/
-   
-   
-   let  msg = "input student marks:"; //user to input marks
-   let a = prompt(msg);
-   if(a>79)// conditional statement which prints grade if fufilled
-       {//if marks > 79 --A
-         console.log('Grade - A');}
-   else if(a<=79 && a>60)
-       {//60<marks60=79  --B
-         console.log('Grade - B');}
-   else if(a>59 && a<=49)
-       {//59<marks<=49 --C
-         console.log('Grade - C');}
-   else if(a>49 && a<=40)
-       {//40<marks<=49  --D
-         console.log('Grade - D');}
-   
-   else if(a<=40)
-   {//less than 40 student gerts E grade
-    console.log('Grade - E');
+const promptMarks = require(`prompt-sync`)();
+// const promptBasicSalary = require(`prompt-sync`)();
+let a = promptMarks("Enter Marks:")
+// let  msg = "input student marks:"; //user to input marks
+// let a = prompt(msg);
+if(a>79)// conditional statement which prints grade if fufilled
+    {//if marks > 79 --A
+      console.log('Grade - A');}
+else if(a<=79 && a>60)
+    {//60<marks60=79  --B
+      console.log('Grade - B');}
+else if(a>59 && a<=49)
+    {//59<marks<=49 --C
+      console.log('Grade - C');}
+else if(a>49 && a<=40)
+    {//40<marks<=49  --D
+      console.log('Grade - D');}
+
+else if(a<=40)
+{//less than 40 student gerts E grade
+ console.log('Grade - E');
 }
 
 
@@ -32,23 +35,27 @@ For example, if the speed is 80, it should print: “Points: 2”. If the driver
 
 
 function checkSpeed(vSpeed){
-const sLimit = 70
-const sRate = 5
-if (vSpeed <= sLimit){    // this statement sets the condition for a vehicle travelling within the limits
-    return "Okay";//   output of above condition
-}
-const overSpeed = vSpeed -sLimit; // to get the value above speed limit
-
-let demeritPoints = 0;
-for (let s = 0; s< overSpeed; s+= sRate){
-    demeritPoints += 1
-    if (demeritPoints > 12){
-        return "License Suspended";
+    const sLimit = 70
+    const sRate = 5
+    if (vSpeed <= sLimit){    // this statement sets the condition for a vehicle travelling within the limits
+        return "Okay";//   output of above condition
     }
-}
-return `points: ${demeritPoints}`;
-} 
-console.log(checkSpeed(95));// calls the function to display speed and response
+    const overSpeed = vSpeed -sLimit; // to get the value above speed limit
+    
+    let demeritPoints = 0;
+    for (let s = 0; s< overSpeed; s+= sRate){
+        demeritPoints += 1
+        if (demeritPoints > 12){
+            return "License Suspended";
+        }
+    }
+    return `points: ${demeritPoints}`;
+    } 
+    console.log(checkSpeed(135));// calls the function to display speed and response
+     
+// challenge 3
+
+
 
 
 
